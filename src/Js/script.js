25688cd29b48099bd038b569a0f47d331df732e0
inmisin <omeroflu2@gmail.com>
2022-11-11 01:41:50 +0300

    Twitter Clone v0.3

--- src/Js/script.js
+++
@@ -1,8 +0,0 @@
-function Test() {
-    var a = document.getElementsByClassName("dropdown-content");
-    if (a.children.style.display === 'none') {
-        a.children.style.display = 'block';
-    }else{
-        a.children.style.display = 'none';
-    }
-}
